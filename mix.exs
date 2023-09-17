ab3edababbdc5e4dc95c9bdb3295d7b99e165fdf
Ihor Katkov <ihorkatkov@gmail.com>
2023-09-17 17:50:03 +0200

    fix dialyzer job

--- mix.exs
+++ mix.exs
@@ -52,7 +52,7 @@ defmodule Geolocator.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false}
     ]
   end
 
