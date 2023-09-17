95a4a0e4cc1d400d4177d6d50c2f2b25d0c83452
Ihor Katkov <ihorkatkov@gmail.com>
2023-09-17 15:58:31 +0200

    remove mailer

--- lib/geolocator_web/router.ex
+++ lib/geolocator_web/router.ex
@@ -22,7 +22,6 @@ defmodule GeolocatorWeb.Router do
       pipe_through [:fetch_session, :protect_from_forgery]
 
       live_dashboard "/dashboard", metrics: GeolocatorWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
   end
 end
