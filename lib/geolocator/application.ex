b688207ceb9baa2088e2ae8b866c9628c154387c
Ihor Katkov <ihorkatkov@gmail.com>
2023-09-17 15:38:09 +0200

    initial commit

---
+++ lib/geolocator/application.ex
@@ -0,0 +1,38 @@
+defmodule Geolocator.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Start the Telemetry supervisor
+      GeolocatorWeb.Telemetry,
+      # Start the Ecto repository
+      Geolocator.Repo,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: Geolocator.PubSub},
+      # Start Finch
+      {Finch, name: Geolocator.Finch},
+      # Start the Endpoint (http/https)
+      GeolocatorWeb.Endpoint
+      # Start a worker by calling: Geolocator.Worker.start_link(arg)
+      # {Geolocator.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Geolocator.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    GeolocatorWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
---
+++ lib/geolocator/mailer.ex
@@ -0,0 +1,3 @@
+defmodule Geolocator.Mailer do
+  use Swoosh.Mailer, otp_app: :geolocator
+end
