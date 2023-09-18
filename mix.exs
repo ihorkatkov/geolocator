f2b5137b6f3d12e9c125d89edabba650f70903bc
Ihor Katkov <ihorkatkov@gmail.com>
2023-09-18 17:31:52 +0200

    chore: add documenation with improvements to the project setup (#9)

--- mix.exs
+++ mix.exs
@@ -69,7 +69,7 @@ defmodule Geolocator.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup"],
+      setup: ["deps.get", "ecto.setup", "eval Geolocator.Release.prepopulate"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
